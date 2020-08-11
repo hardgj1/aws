efcd6c3dcd5eb858db1df6142452b8d8cba5408b
Noah Gift <noah.gift@gmail.com>
2020-08-11 16:55:20 +0000

    adding working version

--- telco.py
+++ telco.py
@@ -1,7 +1,31 @@
 import wikipedia
+import nexmo
+import click
 
-def company(name):
-    text = wikipedia.summary(name, sentences=1)
-    return text
+def api(cname, key, secret, nfrom, nto):
+    ctext = wikipedia.summary(cname, sentences=1)
+    client = nexmo.Client(key=key, secret=secret)
+    result = client.send_message({
+        'from': nfrom,
+        'to': nto,
+        'text': ctext,
+    })
+    print(f"Sent message result: {result}")
+    return result
 
-print(company("google"))
+@click.command()
+@click.option('--cname')
+@click.option('--key')
+@click.option('--secret')
+@click.option('--nfrom')
+@click.option('--nto')
+def callapi(cname, key, secret, nfrom, nto):
+    print("About to call API")
+    results = api(cname, key, secret, nfrom, nto)
+    print(f"Sent API Call from CLI: {results}")
+    
+if __name__ == '__main__':
+    #pylint: disable=no-value-for-parameter
+    callapi()
+
+
